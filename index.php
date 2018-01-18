f6607660555b2e9bdebe4bfd893a4717e76ed442
Jaeyoung Jung <jungj@mit.edu>
2018-01-18 13:36:12 -0500

    Basic login test

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 	include("oidc.php");
-	$state=md5(rand());
-	$nonce=md5(rand());
-	setcookie("state",$state);
-	setcookie("nonce",$nonce);
+	$session=array();
+	$session["state"]=md5(rand());
+	$session["nonce"]=md5(rand());
+	setcookie("session",serialize($session));
 ?>
 
 <!doctype html>
@@ -14,7 +14,7 @@
 <body>
 	<h1>OpenID Connect Test</h1>
 	<a href="https://oidc.mit.edu/authorize?<?php 
-		echo "client_id=".CLIENT_ID."&response_type=code&scope=openid&redirect_uri=".urlencode("https://jungj.scripts.mit.edu:444/dtp/login.php")."&state=".$state."&nonce=".$nonce;
+		echo "client_id=".CLIENT_ID."&response_type=code&scope=openid%20profile%20email&redirect_uri=".urlencode("https://jungj.scripts.mit.edu:444/dtp/login.php")."&state=".$session["state"]."&nonce=".$session["nonce"];
 	?>">Test</a>
 </body>
 </html>
--- login.php
+++ login.php
@@ -6,19 +6,41 @@
 	}else{
 		echo "state variables do not match";
 	}*/
+	$session=unserialize($_COOKIE["session"]);
 	$post=array(
 		"grant_type"=>"authorization_code",
 		"code"=>$_GET["code"],
 		"redirect_uri"=>"https://jungj.scripts.mit.edu:444/dtp/login.php"
 	);
-	if($_COOKIE["state"]==$_GET["state"]){
+	if($session["state"]==$_GET["state"]){
+		echo "<p>Step 1 OK</p>";
 		$curl_req=curl_init("https://oidc.mit.edu/token");
 		curl_setopt($curl_req,CURLOPT_RETURNTRANSFER,true);
 		curl_setopt($curl_req,CURLOPT_USERPWD,CLIENT_ID.":".CLIENT_SECRET);
 		curl_setopt($curl_req,CURLOPT_POSTFIELDS,http_build_query($post));
 		$response=curl_exec($curl_req);
+		$response=json_decode($response,true);
 		curl_close($curl_req);
-		//var_dump($response);
+		echo "<p>Step 2 OK</p>";
+		if(isset($response["id_token"])){
+			$id_token=explode(".",$response["id_token"]);
+			$id_token_body=json_decode(base64_decode($id_token[1]),true);
+			if($session["nonce"]==$id_token_body["nonce"]){
+				$curl_req=curl_init("https://oidc.mit.edu/userinfo");
+				curl_setopt($curl_req,CURLOPT_RETURNTRANSFER,true);
+				//echo $response["access_token"];
+				curl_setopt($curl_req,CURLOPT_HTTPHEADER,array("Authorization: Bearer ".$response["access_token"]));
+				$userinfo=curl_exec($curl_req);
+				$userinfo=json_decode($userinfo,true);
+				curl_close($curl_req);
+				echo "User info:<br>";
+				var_dump($userinfo);
+			}else{
+				echo "<p>nonce variables do not match</p>";
+			}
+		}else{
+			print "<p>No ID token</p>";
+		}
 	}else{
 		echo "state variables do not match";
 	}
