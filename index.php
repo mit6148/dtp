0d0a53634170ff725b2a1e060cde9405fee016cc
AZ <alvinzhu33@yahoo.com>
2018-01-18 18:54:52 -0500

    Started page layout

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 <head>
 	<title>OIDC Test</title>
 	<link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
 	<script
   src="https://code.jquery.com/jquery-3.1.1.min.js"
   integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
@@ -18,9 +19,18 @@
 	<script src="semantic/dist/semantic.min.js"></script>
 </head>
 <body>
-	<h1>OpenID Connect Test</h1>
-	<a href="https://oidc.mit.edu/authorize?<?php
-		echo "client_id=".CLIENT_ID."&response_type=code&scope=openid%20profile%20email&redirect_uri=".urlencode("https://jungj.scripts.mit.edu:444/dtp/login.php")."&state=".$session["state"]."&nonce=".$session["nonce"];
-	?>">Test</a>
+	<div class="ui sidebar vertical menu visible" id="navbar">
+    <h1 class="item">
+      Dumb & Lonely
+    </h1>
+		<a class="item" href="https://oidc.mit.edu/authorize?<?php
+			echo "client_id=".CLIENT_ID."&response_type=code&scope=openid%20profile%20email&redirect_uri=".urlencode("https://jungj.scripts.mit.edu:444/dtp/login.php")."&state=".$session["state"]."&nonce=".$session["nonce"];
+		?>">Test</a>
+  </div>
+	<div class="container">
+		<h1>Welcome to Dumb & Lonely</h1>
+		<h3>OpenID Test</h3>
+		<p>Click "Test" on the side</p>
+	</div>
 </body>
 </html>
