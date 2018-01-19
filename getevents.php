72885e59fbc488639a21f6cbbffbc7dcce00b238
Jaeyoung Jung <jaeyoung.jung@protonmail.com>
2018-01-19 18:25:31 -0500

    get events

---
+++ getevents.php
@@ -0,0 +1,16 @@
+<?php
+	include("db.php");
+
+	$start_available_time = strtotime($_GET["available_date"] . $_GET["start_available_time"]);
+	$end_available_time = strtotime($_GET["available_date"] . $_GET["end_available_time"]);
+	$stmt = $db->prepare("SELECT * FROM events WHERE course LIKE ? AND assignment LIKE ? AND location LIKE ? AND start_time < ? AND end_time > ?");
+	$stmt->execute(array(
+		"%" . $_GET["course"] . "%",
+		"%" . $_GET["assignment"] . "%",
+		"%" . $_GET["location"] . "%",
+		$end_available_time,
+		$start_available_time
+	));
+	$results = $stmt->fetchAll(PDO::FETCH_KEY_PAIR);
+	echo json_encode($results);
+?>
