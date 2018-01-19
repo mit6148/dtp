2fdff12835fc0a6eff46be977a750b04dcac0ad9
Jaeyoung Jung <jaeyoung.jung@protonmail.com>
2018-01-18 20:57:27 -0500

    Prettify

--- index.php
+++ index.php
@@ -3,20 +3,20 @@
 	include("db.php");
 	
 	$logged_in=false;
-	if(isset($_COOKIE["session_uid"])){
-		$stmt=$db->prepare("SELECT * FROM sessions WHERE uid = ?");
+	if (isset($_COOKIE["session_uid"])) {
+		$stmt = $db->prepare("SELECT * FROM sessions WHERE uid = ?");
 		$stmt->bindParam(1,$uid);
-		$uid=$_COOKIE["session_uid"];
+		$uid = $_COOKIE["session_uid"];
 		$stmt->execute();
-		$result=$stmt->setFetchMode(PDO::FETCH_ASSOC);
-		if($stmt->rowCount()>0){
-			$session=$stmt->fetch();
-			if($session["expire_time"]>time()){
-				$logged_in=true;
+		$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+		if ($stmt->rowCount() > 0) {
+			$session = $stmt->fetch();
+			if($session["expire_time"] > time()) {
+				$logged_in = true;
 			}
 		}
 	}
-	if(!logged_in){
+	if (!logged_in) {
 		$state=md5(rand());
 		$nonce=md5(rand());
 		setcookie("state",$state);
@@ -31,10 +31,10 @@
 <body>
 
 	<?php
-		if($logged_in){ ?>
+		if ($logged_in) { ?>
 		<h1>You are logged in!</h1>
 		<a href="logout.php">Click here to log out</a>
-	<?php }else{ ?>
+	<?php } else { ?>
 		<h1>Log in</h1>
 	<a href="https://oidc.mit.edu/authorize?<?php
 		echo "client_id=".CLIENT_ID."&response_type=code&scope=openid%20profile%20email&redirect_uri=".urlencode("https://jungj.scripts.mit.edu:444/dtp/login.php")."&state=".$state."&nonce=".$nonce;
--- login.php
+++ login.php
@@ -1,43 +1,38 @@
 <?php
 	include("oidc.php");
 	include("db.php");
-	if(!(isset($_COOKIE["state"]) && isset($_COOKIE["nonce"]))){
+	if (!(isset($_COOKIE["state"]) && isset($_COOKIE["nonce"]))) {
 		echo("cookies not set");
-//		header("Location: https://jungj.scripts.mit.edu:444/dtp/");
 	}
-	$post_array=array(
+	$post_array = array(
 		"grant_type"=>"authorization_code",
 		"code"=>$_GET["code"],
 		"redirect_uri"=>"https://jungj.scripts.mit.edu:444/dtp/login.php"
 	);
-	if($_COOKIE["state"]==$_GET["state"]){
-		$ch=curl_init("https://oidc.mit.edu/token");
-		curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-		curl_setopt($ch,CURLOPT_USERPWD,CLIENT_ID.":".CLIENT_SECRET);
-		curl_setopt($ch,CURLOPT_POSTFIELDS,http_build_query($post_array));
-		$response=curl_exec($ch);
-		$response=json_decode($response,true);
+	if ($_COOKIE["state"] == $_GET["state"]) {
+		$ch = curl_init("https://oidc.mit.edu/token");
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_USERPWD,CLIENT_ID . ":" . CLIENT_SECRET);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_array));
+		$response = curl_exec($ch);
+		$response = json_decode($response,true);
 		curl_close($ch);
-//		var_dump($response);
-		if(isset($response["id_token"])){
-			$id_token=explode(".",$response["id_token"]);
-			$id_token_body=json_decode(base64_decode($id_token[1]),true);
-//			var_dump($id_token_body);
-			if($_COOKIE["nonce"]==$id_token_body["nonce"]){
-				$ch=curl_init("https://oidc.mit.edu/userinfo");
-				curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-				curl_setopt($ch,CURLOPT_HTTPHEADER,array("Authorization: Bearer ".$response["access_token"]));
-				$userinfo=curl_exec($ch);
-				$userinfo=json_decode($userinfo,true);
-
-			    $stmt=$db->prepare("SELECT * FROM users WHERE sub = ?");
-		    	$stmt->bindParam(1,$userinfo["sub"]);
+		if (isset($response["id_token"])) {
+			$id_token = explode(".", $response["id_token"]);
+			$id_token_body = json_decode(base64_decode($id_token[1]), true);
+			if ($_COOKIE["nonce"] == $id_token_body["nonce"]) {
+				$ch = curl_init("https://oidc.mit.edu/userinfo");
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: Bearer ".$response["access_token"]));
+				$userinfo = curl_exec($ch);
+				$userinfo = json_decode($userinfo,true);
+			    $stmt = $db->prepare("SELECT * FROM users WHERE sub = ?");
+		    	$stmt->bindParam(1, $userinfo["sub"]);
 			    $stmt->execute();
-				$result=$stmt->setFetchMode(PDO::FETCH_ASSOC);
-				//$user=$stmt->fetch();
-			    if(isset($userinfo["preferred_username"]) && isset($userinfo["email"])){
-			    	if($stmt->rowCount()==0){
-				    	$insert_stmt=$db->prepare("INSERT INTO users (sub, kerberos, email, name) VALUES (?, ?, ?, ?)");
+				$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+			    if (isset($userinfo["preferred_username"]) && isset($userinfo["email"])) {
+			    	if ($stmt->rowCount() == 0) {
+				    	$insert_stmt = $db->prepare("INSERT INTO users (sub, kerberos, email, name) VALUES (?, ?, ?, ?)");
 				    	$insert_stmt->execute(array(
 				    		$userinfo["sub"],
 				    		$userinfo["preferred_username"],
@@ -45,25 +40,25 @@
 				    		$userinfo["name"]
 				    	));
 			    	}
-			    	$user=$stmt->fetch();
-			    	$session_stmt=$db->prepare("INSERT INTO sessions (uid, sub, expire_time) VALUES (?, ?, ?)");
-			    	$session_uid=md5($userinfo["sub"].(string)(time()*rand()));
+			    	$user = $stmt->fetch();
+			    	$session_stmt = $db->prepare("INSERT INTO sessions (uid, sub, expire_time) VALUES (?, ?, ?)");
+			    	$session_uid = md5($userinfo["sub"] . (string)(time()*rand()));
 			    	$session_stmt->execute(array(
 			    		$session_uid,
 			    		$userinfo["sub"],
 			    		time()+60*60*24*30
 			    	));
-			    	setcookie("session_uid",$session_uid,time()+60*60*24*90);
+			    	setcookie("session_uid", $session_uid, time()+60*60*24*90);
 			    	header("Location: https://jungj.scripts.mit.edu:444/dtp/");
-				}else{
+				} else {
 					echo "Not all scopes enabled";
 				}
-			}else{
+			} else {
 				echo "nonce does not match";
 			}
-		}else{
+		} else {
 			echo "No id_token";
 		}
-	}else{
+	} else {
 		echo "state does not match";
 	}
