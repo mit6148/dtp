3d18f64051c3fa1be7da1a85493b7a2b5c3336b6
Jaeyoung Jung <jaeyoung.jung@protonmail.com>
2018-01-20 10:59:44 -0500

    Add functions to event.php and user.php

--- event.php
+++ event.php
@@ -6,4 +6,12 @@
 			$event_id
 		));
 	}
+	function get_eventinfo($db, $event_id) {
+		$stmt = $db->prepare("SELECT * FROM events WHERE id = ?");
+		$stmt->execute(array(
+			$event_id
+		));
+		$result = $stmt->fetch(PDO::FETCH_ASSOC);
+		return $result;
+	}
 ?>
--- user.php
+++ user.php
@@ -15,10 +15,30 @@
 		$db->query("DELETE FROM `logins` WHERE expire_time < UNIX_TIMESTAMP()");
 		$stmt = $db->prepare("SELECT * FROM logins WHERE uid = ?");
 		$stmt->execute(array($login_uid));
-		$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+		//$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
 		if ($stmt->rowCount() > 0) {
 			return true;
 		}
 		return false;
+	}
+	function get_user_owned_events($db, $sub) {
+		$stmt = $db->query("SELECT * FROM events WHERE owner_sub = ?");
+		$stmt->execute(array(
+			$sub
+		));
+		$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		return $results;
+	}
+	function get_user_signedup_events($db, $sub) {
+		$stmt = $db->query("SELECT event_id FROM signups WHERE user_sub = ?");
+		$stmt->execute(array(
+			$sub
+		));
+		$event_ids = $stmt->fetchAll(PDO::FETCH_NUM);
+		$results = array();
+		foreach ($event_ids as $event_id) {
+			$results[] = get_eventinfo($event_id[0]);
+		}
+		return $results;
 	}
 ?>
