6e2a99d955bd4f38b451cfd914c4e68809a43cae
Jaeyoung Jung <jaeyoung.jung@protonmail.com>
2018-01-19 22:50:03 -0500

    "This might work" -Jaeyoung Jung 2018

--- index.js
+++ index.js
@@ -12,41 +12,44 @@ $('#submitEvent').form({
 );
 $('#submitEvent').submit(function(event) {
     //console.log("hello");
-    var input = {
-        'course' : $('input[name=submit_course]').val(),
-        'assignment' : $('input[name=submit_assignment]').val(),
-        'location' : $('input[name=submit_location]').val(),
-        'date' : $('input[name=submit_date]').val(),
-        'start_time' : $('input[name=submit_start_time]').val(),
-        'end_time' : $('input[name=submit_end_time]').val(),
-    };
-    $.ajax({
-        type: 'POST',
-        url: 'addevent.php',
-        data : input,
-        cache: false,
-    })
-    .done(function() {
-        $('#messages').prepend('<div class="ui success compact message"><i class="close icon"></i><div class="header">Event received!</div><p>Other students will be able to see and add your event.</p></div>');
-        $('.message .close').off('click');
-        $('.message .close').on('click', function() {
-            $(this)
-              .closest('.message')
-              .transition('fade')
-            ;
+    $('#submitEvent').form('onSuccess', function() {
+        var input = {
+            'course' : $('input[name=submit_course]').val(),
+            'assignment' : $('input[name=submit_assignment]').val(),
+            'location' : $('input[name=submit_location]').val(),
+            'date' : $('input[name=submit_date]').val(),
+            'start_time' : $('input[name=submit_start_time]').val(),
+            'end_time' : $('input[name=submit_end_time]').val(),
+        };
+        $.ajax({
+            type: 'POST',
+            url: 'addevent.php',
+            data : input,
+            cache: false,
+        })
+        .done(function() {
+            $('#messages').prepend('<div class="ui success compact message"><i class="close icon"></i><div class="header">Event received!</div><p>Other students will be able to see and add your event.</p></div>');
+            $('.message .close').off('click');
+            $('.message .close').on('click', function() {
+                $(this)
+                  .closest('.message')
+                  .transition('fade')
+                ;
+            });
+        })
+        .fail(function() {
+            $('#messages').prepend('<div class="ui error compact message"><i class="close icon"></i><div class="header">Your event was not received.</div><p>If the problem persists, contact a Network Adminstrator.</p></div>');
+            $('.message .close').off('click');
+            $('.message .close').on('click', function() {
+                $(this)
+                  .closest('.message')
+                  .transition('fade')
+                ;
+            });
         });
-    })
-    .fail(function() {
-        $('#messages').prepend('<div class="ui error compact message"><i class="close icon"></i><div class="header">Your event was not received.</div><p>If the problem persists, contact a Network Adminstrator.</p></div>');
-        $('.message .close').off('click');
-        $('.message .close').on('click', function() {
-            $(this)
-              .closest('.message')
-              .transition('fade')
-            ;
-        });
-    })
-    ;
+        $('.ui.modal').modal('hide');
+        $('#submitEvent').form('reset');
+    });
+    $('#submitEvent').form('validate form');
     event.preventDefault();
-    $('#submitEvent').form('reset');
 });
