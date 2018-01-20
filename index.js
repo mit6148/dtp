75c0c546e97c9a9d4190a0ac678b6c77a2aae423
Nikhil Reddy <nikhilreddy6547@gmail.com>
2018-01-19 22:15:53 -0500

    body adding

--- index.js
+++ index.js
@@ -36,10 +36,10 @@ $('#submitEvent').submit(function(event) {
         cache: false,
     })
     .done(function() {
-        $('#main').append('<div class="ui success message"><i class="close icon"></i><div class="header">Event received!</div><p>Other students will be able to see and add your event.</p></div>');
+        $('body').append('<div class="ui success message"><i class="close icon"></i><div class="header">Event received!</div><p>Other students will be able to see and add your event.</p></div>');
     })
     .fail(function() {
-        $('#main').append('<div class="ui error message"><i class="close icon"></i><div class="header">Your event was not received.</div><p>If the problem persists, contact a Network Adminstrator.</p></div>');
+        $('body').append('<div class="ui error message"><i class="close icon"></i><div class="header">Your event was not received.</div><p>If the problem persists, contact a Network Adminstrator.</p></div>');
     })
     ;
     event.preventDefault();
