842b3097a5a07247dbe4914384822cd4947e1570
Jaeyoung Jung <jaeyoung.jung@protonmail.com>
2018-01-20 11:58:27 -0500

    messages

--- signup_event.js
+++ signup_event.js
@@ -6,6 +6,24 @@ function signup(event_id) {
 			'event_id': event_id
 		},
 		cache: false
-	}).done(console.log)
-	.fail(console.log);
+	}).done(function() {
+		$('#messages').prepend('<div class="ui success compact message"><i class="close icon"></i><div class="header">Event added to schedule!</div></div>');
+        $('.message .close').off('click');
+        $('.message .close').on('click', function() {
+            $(this)
+              .closest('.message')
+              .transition('fade')
+            ;
+        });
+	})
+	.fail(function() {
+        $('#messages').prepend('<div class="ui error compact message"><i class="close icon"></i><div class="header">Your request was not received by the server.</div><p>If the problem persists, contact a Network Adminstrator.</p></div>');
+        $('.message .close').off('click');
+        $('.message .close').on('click', function() {
+            $(this)
+              .closest('.message')
+              .transition('fade')
+            ;
+        });
+    });
 }
