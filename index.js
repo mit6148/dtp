7695e827019bb865cf611255c67fecd7e1fb6322
Jaeyoung Jung <jaeyoung.jung@protonmail.com>
2018-01-19 22:40:02 -0500

    Fix messages

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
 $('#addEvent').on('click', function() {
     $('.ui.modal').modal('show');
 });
-/*$('#submitEvent').form({
-    fields: {
-        submit_course: {
-            identifier: 'submit_course',
-            rules: [
-              {
-                type   : 'empty',
-                prompt : 'Please enter your course'
-              }
-            ]
-          },
+$('#submitEvent').form({
+        submit_course: 'empty',
         submit_assignment: 'empty',
         submit_location: 'empty',
         submit_date: 'empty',
         submit_start_time: 'empty',
         submit_end_time: 'empty'
     }
-});*/
+});
 $('#submitEvent').submit(function(event) {
     //console.log("hello");
     var input = {
@@ -37,17 +28,25 @@ $('#submitEvent').submit(function(event) {
     })
     .done(function() {
         $('#messages').prepend('<div class="ui success compact message"><i class="close icon"></i><div class="header">Event received!</div><p>Other students will be able to see and add your event.</p></div>');
+        $('.message .close').off('click');
+        $('.message .close').on('click', function() {
+            $(this)
+              .closest('.message')
+              .transition('fade')
+            ;
+        });
     })
     .fail(function() {
         $('#messages').prepend('<div class="ui error compact message"><i class="close icon"></i><div class="header">Your event was not received.</div><p>If the problem persists, contact a Network Adminstrator.</p></div>');
+        $('.message .close').off('click');
+        $('.message .close').on('click', function() {
+            $(this)
+              .closest('.message')
+              .transition('fade')
+            ;
+        });
     })
     ;
     event.preventDefault();
     $('#submitEvent').form('reset');
-});
-$('.message .close').on('click', function() {
-    $(this)
-      .closest('.message')
-      .transition('fade')
-    ;
 });
