898d8029faa8f6c9f1b132cfc26c60fee5dcf516
Nikhil Reddy <nikhilreddy6547@gmail.com>
2018-01-19 21:21:50 -0500

    fix

--- index.php
+++ index.php
@@ -171,22 +171,23 @@
 		  </div>
 		</form>
 		<script>
-			$('#submitEvent').submit(function() {
-				console.log("hello");
-					var input = {
-							'course' : $('input[name=submit_course]').val(),
-							'assignment' : $('input[name=submit_assignment]').val(),
-							'location' : $('input[name=submit_location]').val(),
-							'date' : $('input[name=submit_date]').val(),
-							'start_time' : $('input[name=submit_start_time]').val(),
-							'end_time' : $('input[name=submit_end_time]').val(),
-					};
-					$.ajax({
-						type: 'POST',
-						url: 'addevent.php',
-						data : input,
-						cache: false,
-					});
+			$('#submitEvent').submit(function(event) {
+				//console.log("hello");
+				var input = {
+						'course' : $('input[name=submit_course]').val(),
+						'assignment' : $('input[name=submit_assignment]').val(),
+						'location' : $('input[name=submit_location]').val(),
+						'date' : $('input[name=submit_date]').val(),
+						'start_time' : $('input[name=submit_start_time]').val(),
+						'end_time' : $('input[name=submit_end_time]').val(),
+				};
+				$.ajax({
+					type: 'POST',
+					url: 'addevent.php',
+					data : input,
+					cache: false,
+				});
+				event.preventDefault();
 			});
 		</script>
 	</div>
