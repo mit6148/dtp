7845af9866c38866d41c2cb6df628377b038e90a
Jaeyoung Jung <jungj@mit.edu>
2018-01-20 02:17:24 -0500

    Fix datetime parsing

--- get_events.php
+++ get_events.php
@@ -2,9 +2,17 @@
 	include("db.php");
 	include("user.php");
 
-	$start_available_time = strtotime($_GET["available_date"] . $_GET["start_available_time"]);
-	$end_available_time = strtotime($_GET["available_date"] . $_GET["end_available_time"]);
+	//echo $_GET["available_date"] . " " . $_GET["start_available_time"];
+	$start_available_datetime = date_create_from_format("m-d-Y h:i a", $_GET["available_date"] . " " . $_GET["start_available_time"]);
+	$start_available_time = date_timestamp_get($start_available_datetime);
+	$end_available_datetime = date_create_from_format("m-d-Y h:i a", $_GET["available_date"] . " " . $_GET["end_available_time"]);
+	$end_available_time = date_timestamp_get($end_available_datetime);
+/*	if ($start_available_time === false) {
+		echo "failed";
+	}*/
 	$stmt = $db->prepare("SELECT * FROM events WHERE course LIKE ? AND assignment LIKE ? AND location LIKE ? AND start_time < ? AND end_time > ?");
+	echo $start_available_time;
+	echo $end_available_time;
 	$stmt->execute(array(
 		"%" . $_GET["course"] . "%",
 		"%" . $_GET["assignment"] . "%",
@@ -14,9 +22,9 @@
 	));
 	$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-	$userinfo = get_userinfo($db, $results["owner_sub"]);
+/*	$userinfo = get_userinfo($db, $results["owner_sub"]);
 	$results["name"] = $userinfo["name"];
-	$results["email"] = $userinfo["email"];
-	
+	$results["email"] = $userinfo["email"];*/
+
 	echo json_encode($results);
 ?>
