f837a2a2a85e30f68d4fe3aaa125b93884531cec
Jaeyoung Jung <jaeyoung.jung@protonmail.com>
2018-01-20 22:49:49 -0500

    remove that code

--- get_events.js
+++ get_events.js
@@ -61,9 +61,9 @@ function parseTime(date) {
     return hours + ":" + minutes.substr(-2) + day;
 }
 function parseEvent(event) {
-    /*let start_time = new Date(event.start_time * 1000);
-    let start_date = (new Date(event.start_time * 1000 - 3600 * 1000 * 4)).toDateString().split(" ");
-    let end_time = new Date(event.end_time * 1000);*/
+    let start_time = new Date(event.start_time * 1000);
+    let start_date = (new Date(event.start_time * 1000)).toDateString().split(" ");
+    let end_time = new Date(event.end_time * 1000);
     $('#events').append(('<tr><td>{0}</td><td>{1}</td><td>{2}</td><td>{3}</td><td>{4}</td><td>{5}</td><td>{6}</td>' + ((logged_in) ? '<td id="{7}"></td>' : '') + '</tr>').format(event.course, event.assignment, event.location, start_date[0] + " " + start_date[1] + " " + start_date[2], parseTime(start_time), parseTime(end_time), (event.owner_sub == sub) ? 'You' : ('<a href="mailto:' + event.owner_email + '">' + event.owner_name + '</a>'), event.id));
     eventButton(event.id, event.is_signed_up);
 }
