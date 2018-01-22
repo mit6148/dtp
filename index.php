1394c7c1ab05b7654c5f24f2553f6e070bf73733
AZ <alvinzhu33@yahoo.com>
2018-01-22 13:18:16 -0500

    Button placement

--- index.php
+++ index.php
@@ -65,10 +65,10 @@
 				<div class="item">
 					<?php echo $userinfo["given_name"]; ?>
 				</div>
-				<a class="active-item" id="scheduleLink">
+				<a class="active item" id="scheduleLink">
 		    	My Schedule
 		 		</a>
-				<a class="active-item" href="logout.php" id="logout">
+				<a class="active item" href="logout.php" id="logout">
 					Logout
 				</a>
 			</div>
