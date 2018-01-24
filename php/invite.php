ae7263558f65dcd91016a528498cee99ae8ab045
Jaeyoung Jung <jaeyoung.jung@protonmail.com>
2018-01-24 13:55:35 -0500

    get_invites

--- php/invite.php
+++ php/invite.php
@@ -1,5 +1,5 @@
 <?php
-	//GET: sub, event_id, invitee_kerberos
+	//POST: sub, event_id, invitee_kerberos
 	//returns: 1 if successful, 2 if already invited
 
 	include("oidc.php");
@@ -9,8 +9,8 @@
 
 	$find_stmt = $db->prepare("SELECT EXISTS(SELECT * FROM invites WHERE kerberos = ? AND event_id = ?)");
 	$find_stmt->execute(array(
-		$_GET["invitee_kerberos"],
-		$_GET["event_id"]
+		$_POST["invitee_kerberos"],
+		$_POST["event_id"]
 	));
 	$exists = $find_stmt->fetch(PDO::FETCH_NUM)[0];
 
@@ -19,21 +19,16 @@
 	} else {
 		$stmt = $db->prepare("INSERT INTO invites (kerberos, event_id) VALUES (?, ?)");
 		$stmt->execute(array(
-			$_GET["invitee_kerberos"],
-			$_GET["event_id"]
+			$_POST["invitee_kerberos"],
+			$_POST["event_id"]
 		));
-		$sender_userinfo = get_userinfo($db, $_GET["sub"]);
-		$eventinfo = get_eventinfo($db, $_GET["event_id"]);
-		$recipient_email = $_GET["invitee_kerberos"] . "@mit.edu";
+		$sender_userinfo = get_userinfo($db, $_POST["sub"]);
+		$eventinfo = get_eventinfo($db, $_POST["event_id"]);
+		$recipient_email = $_POST["invitee_kerberos"] . "@mit.edu";
 		$subject = $sender_userinfo["given_name"] . " invited you to work on " . $eventinfo["course"] . ": " . $eventinfo["assignment"];
 		$body = '<a href="' . INDEX_URL . '">Click here to login and view your invitation.</a>';
 		$header = "From: noreply\n";
 		$header .= "Content-Type: text/html; charset=ISO-8859-1";
-		echo $recipient_email;
-		echo $subject;
-		echo $body;
-		echo $header;
-		echo "1";
 		mail($recipient_email, $subject, $body, $header);
 	}
 
