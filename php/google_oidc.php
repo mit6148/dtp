ace6a748fe0fa9cc7dca6560242c2d8ea0ba455a
Jaeyoung Jung <jungj@mit.edu>
2018-01-24 02:47:49 -0500

    delete oidc credentials file

--- php/google_oidc.php
+++
@@ -1,6 +0,0 @@
-<?php
-	define("GOOGLE_CLIENT_ID", "39147957521-46jo94r45gqhdeahejovf48ojpht5dt1.apps.googleusercontent.com");
-	define("GOOGLE_CLIENT_SECRET", "FiGCzvJSKQVbU8_YYS0FXInK");
-	define("GOOGLE_LOGIN_PAGE_URL", "https://jungj.scripts.mit.edu/dtp_testing/google_login.php");
-	define("GOOGLE_INDEX_URL", "https://jungj.scripts.mit.edu/dtp_testing/");
-?>
