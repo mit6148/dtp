949c95e6f01ff6e370e1e9237c4e43d6a87568b2
Jaeyoung Jung <jungj@mit.edu>
2018-01-25 11:36:34 -0500

    email formatting

--- php/invite.php
+++ php/invite.php
@@ -34,9 +34,9 @@
 			$eventinfo = get_eventinfo($db, $_POST["event_id"]);
 			$recipient_email = $email;
 			$sender_email = "noreply";
-			$subject = $sender_userinfo["name"] . " invited you to work on " . $eventinfo["course"] . ": " . $eventinfo["assignment"] . "through dtp";
-			$body = $sender_userinfo["name"] . "invited you to work on " . $eventinfo["course"] . ": " . $eventinfo["assignment"] . " on " . date("l F j, Y", $eventinfo["start_time"]) . " from " . date("g:i A", $eventinfo["start_time"]) . " to " . date("g:i A", $eventinfo["end_time"]) . " at " . $eventinfo["location"] ".<br>\n"
-			$body = '<a href="' . INDEX_URL . '">Click here to login and view your invitation.</a>';
+			$subject = $sender_userinfo["name"] . " invited you to work on " . $eventinfo["course"] . ": " . $eventinfo["assignment"];
+			$body = $sender_userinfo["name"] . " invited you to work on " . $eventinfo["course"] . ": " . $eventinfo["assignment"] . " on " . date("l F j, Y", $eventinfo["start_time"]) . " from " . date("g:i A", $eventinfo["start_time"]) . " to " . date("g:i A", $eventinfo["end_time"]) . " at " . $eventinfo["location"] . ".<br>\n";
+			$body .= '<a href="' . INDEX_URL . '">Click here to login and view your invitation.</a>';
 			$header = "From: " . $sender_email . "\n";
 			$header .= "Content-Type: text/html; charset=utf-8";
 			mail($recipient_email, $subject, $body, $header);
