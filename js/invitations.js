efd5164dcaca3509324772346b57cc193b77ef46
AZ <alvinzhu33@yahoo.com>
2018-01-26 12:34:41 -0500

    Invitations look

--- js/invitations.js
+++ js/invitations.js
@@ -16,7 +16,7 @@ function populateInvitations(invitations) {
 	if (invitations.length === 0) {
 		$('#invitationsBody').html('<div class="ui center aligned segment">No invitations</div>');
 	} else {
-		$('#invitationsBody').html('<div class="ui aligned segments" id="segBody"></div>');
+		$('#invitationsBody').html('<div id="segBody"></div>');
 	}
 	invitations.sort(ByStartTime);
 	for (i in invitations) {
@@ -26,14 +26,14 @@ function populateInvitations(invitations) {
 
 function addToInvitationsTable(invitation) {
 	//console.log(invitation);
-	str = "";
-	str += '<div class="ui segment">Invitation from <b>' + invitation.inviter.name + '</b> for <b>' + invitation.event.course + '</b>: <b>' + invitation.event.assignment + '</b><div class="ui right floated red button" style="margin-top:-11px" onclick="dismissInvitation(' + invitation.event_id + ')">Dismiss</div><div class="ui right floated green button invitationButton" style="margin-top:-11px" onclick="acceptInvitation(' + invitation.event_id + ')">Accept</div></div><div class="ui segments">';
+	str = "<div class='ui aligned segments'";
+	str += '<div class="ui clearing segment">Invitation from <b>' + invitation.inviter.name + '</b> for <b>' + invitation.event.course + '</b>: <b>' + invitation.event.assignment + '</b><div class="ui right floated red button" onclick="dismissInvitation(' + invitation.event_id + ')">Dismiss</div><div class="ui right floated green button invitationButton" onclick="acceptInvitation(' + invitation.event_id + ')">Accept</div></div><div class="ui segments">';
 	str += '<div class="ui horizontal segments"><div class="ui segment">Location: <b>'+ invitation.event.location + '</b></div></div>';
 	let start_time = new Date(invitation.event.start_time * 1000);
     let start_date = new Date(invitation.event.start_time * 1000).toDateString();
     let end_time = new Date(invitation.event.end_time * 1000);
 	str += '<div class="ui horizontal segments"><div class="ui segment">Date: <b>' + start_date + '</b></div><div class="ui segment">Start Time: <b>' + parseTime(start_time) + '</b></div><div class="ui segment">End Time: <b>'+ parseTime(end_time) + '</b></div></div>';
-	str += '</div>';
+	str += '</div></div>';
 	$('#segBody').append(str);
 }
 
