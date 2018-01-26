8aba6518b159e043e4dc1f3cc892529a46857057
Jaeyoung Jung <jungj@mit.edu>
2018-01-26 11:34:09 -0500

    fix

--- logout.php
+++ logout.php
@@ -6,7 +6,7 @@
 	if (isset($_COOKIE["login"])) {
 		$login_array = unserialize($_COOKIE["login"]);
 		$stmt = $db->prepare("DELETE FROM logins WHERE id = ?");
-		$stmt->execute(array($login_array[0]));
+		$stmt->execute(array($login_array["id"]));
 	}
 	unset($_COOKIE["login"]);
 	setcookie("login", "", time()-3600);
